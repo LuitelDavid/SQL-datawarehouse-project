e624f7d375993379cdfd8dbd1259697038cf7fef
David Luitel <187544562+LuitelDavid@users.noreply.github.com>
2026-03-03 17:17:42 +0545

    Modify init_database.sql for PostgreSQL compatibility
    Updated the script to create the 'DataWarehouse' database and its schemas in PostgreSQL format.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,6 +1,6 @@
 /*
 =============================================================
-Create Database and Schemas
+Create Database and Schemas in postgresql 
 =============================================================
 Script Purpose:
     This script creates a new database named 'DataWarehouse' after checking if it already exists. 
@@ -11,32 +11,19 @@ WARNING:
     Running this script will drop the entire 'DataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
+
+	The code may not run in other databases.
 */
-
-USE master;
-GO
-
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
-GO
 
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-GO
-
-USE DataWarehouse;
-GO
 
 -- Create Schemas
 CREATE SCHEMA bronze;
-GO
+
 
 CREATE SCHEMA silver;
-GO
+
 
 CREATE SCHEMA gold;
-GO
+
